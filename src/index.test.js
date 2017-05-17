f213870b850e96274e0f0e51e932665c1f8b7946
alex.fan <alex.fan@okchem.com>
2017-05-17 17:57:40 +0800

    simple and complex json reader test cases passed

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,15 @@
 import JsonReader from './index'
 
-describe('i18 resource file transformation automation', function () {
-  it('should read json file', function () {
-    const jsonReader = new JsonReader('resources/product.json')
+describe('i18 resource file transformation automation', () => {
+  it('获取 simple.json 里面的一组key value', () => {
+    const jsonReader = new JsonReader('resources/simple.json')
     expect(jsonReader.getJson()['Key1 Key2']).toEqual('Value1')
+  })
+  it('获取 simple.json 里面的一组key value', () => {
+    const jsonReader = new JsonReader('resources/complex.json')
+    expect(jsonReader.getJson()['Key1']).toEqual('Value1')
+    expect(jsonReader.getJson()['Key2']).toEqual('Value2')
+    expect(jsonReader.getJson()['Key3 Key4']).toEqual('Value3 Value3')
+    expect(jsonReader.getJson()['Key5 Key6 Key7']).toEqual('Value4 Value4 Value4')
   })
 })
