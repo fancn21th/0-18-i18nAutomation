6f4fd1e884f71bf05845acbfaec989b2928ff59c
alex.fan <alex.fan@okchem.com>
2017-05-17 18:00:12 +0800

    simple and complex json reader test cases passed - test case typo fix

--- src/index.test.js
+++ src/index.test.js
@@ -5,7 +5,7 @@ describe('i18 resource file transformation automation', () => {
     const jsonReader = new JsonReader('resources/simple.json')
     expect(jsonReader.getJson()['Key1 Key2']).toEqual('Value1')
   })
-  it('获取 simple.json 里面的一组key value', () => {
+  it('获取 complex.json 里面的多组key value', () => {
     const jsonReader = new JsonReader('resources/complex.json')
     expect(jsonReader.getJson()['Key1']).toEqual('Value1')
     expect(jsonReader.getJson()['Key2']).toEqual('Value2')
